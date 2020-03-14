874b313427ddd2ba39390d90a2a79aab08206eac
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-03-14 15:13:38 +0100

    Change 2 while loops into 1 function

--- guessing_game.py
+++ guessing_game.py
@@ -6,32 +6,27 @@ def generate_numbers(start, end, list_name):
         list_name.append(random.randint(start, end))
 
 
-random_ints_1to99 = []
-generate_numbers(1, 99, random_ints_1to99)
-for i in range(10):
-    user_number = int(input("Enter an integer from 1 to 99: "))
-    while random_ints_1to99[i] != user_number:
-        if user_number < random_ints_1to99[i]:
+def check_user_input(user_number, list_of_numbers, i):
+    while list_of_numbers[i] != user_number:
+        if user_number < list_of_numbers[i]:
             print("guess is low")
             user_number = int(input("Enter an integer from 1 to 99: "))
-        elif user_number > random_ints_1to99[i]:
+        elif user_number > list_of_numbers[i]:
             print("guess is high")
             user_number = int(input("Enter an integer from 1 to 99: "))
         else:
             break
     print("you guessed it!")
 
+
+random_ints_1to99 = []
+generate_numbers(1, 99, random_ints_1to99)
+for i in range(10):
+    user_number = int(input("Enter an integer from 1 to 99: "))
+    check_user_input(user_number, random_ints_1to99, i)
+
 random_ints_1to49 = []
 generate_numbers(1, 49, random_ints_1to49)
 for i in range(10):
     user_number = int(input("Enter an integer from 1 to 49: "))
-    while random_ints_1to49[i] != user_number:
-        if user_number < random_ints_1to49[i]:
-            print("guess is low")
-            user_number = int(input("Enter an integer from 1 to 49: "))
-        elif user_number > random_ints_1to49[i]:
-            print("guess is high")
-            user_number = int(input("Enter an integer from 1 to 49: "))
-        else:
-            break
-    print("you guessed it!")
+    check_user_input(user_number, random_ints_1to49, i)
