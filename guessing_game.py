1561db1fee22536734d1409c3153ab1b9d50c004
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-03-14 15:31:54 +0100

    Get rid of magic numbers

--- guessing_game.py
+++ guessing_game.py
@@ -6,14 +6,18 @@ def generate_numbers(start, end, list_name):
         list_name.append(random.randint(start, end))
 
 
-def check_user_input(user_number, list_of_numbers, i):
+def check_user_input(user_number, list_of_numbers, i, range_end):
     while list_of_numbers[i] != user_number:
         if user_number < list_of_numbers[i]:
             print("guess is low")
-            user_number = int(input("Enter an integer from 1 to 99: "))
+            user_number = int(
+                input("Enter an integer from 1 to " + str(range_end) + ": ")
+            )
         elif user_number > list_of_numbers[i]:
             print("guess is high")
-            user_number = int(input("Enter an integer from 1 to 99: "))
+            user_number = int(
+                input("Enter an integer from 1 to " + str(range_end) + ": ")
+            )
         else:
             break
     print("you guessed it!")
@@ -24,13 +28,13 @@ def guess_10_numbers(list_of_numbers, range_end):
         user_number = int(
             input("Enter an integer from 1 to " + str(range_end) + ": ")
         )
-        check_user_input(user_number, list_of_numbers, i)
+        check_user_input(user_number, list_of_numbers, i, range_end)
 
 
 random_ints_1to99 = []
-generate_numbers(1, 99, random_ints_1to99)
-guess_10_numbers(random_ints_1to99, 99)
+generate_numbers(start=1, end=99, list_name=random_ints_1to99)
+guess_10_numbers(random_ints_1to99, range_end=99)
 
 random_ints_1to49 = []
-generate_numbers(1, 49, random_ints_1to49)
-guess_10_numbers(random_ints_1to49, 49)
+generate_numbers(start=1, end=49, list_name=random_ints_1to49)
+guess_10_numbers(random_ints_1to49, range_end=49)
