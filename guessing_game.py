9a0cd237e7ce81f36113b204593447c30bc1130f
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-03-14 15:03:29 +0100

    Extract list appending to function

--- guessing_game.py
+++ guessing_game.py
@@ -1,15 +1,13 @@
 import random
+
+
+def generate_numbers(start, end, list_name):
+    for i in range(10):
+        list_name.append(random.randint(start, end))
+
+
 random_ints_1to99 = []
-random_ints_1to99.append(random.randint(1, 99))
-random_ints_1to99.append(random.randint(1, 99))
-random_ints_1to99.append(random.randint(1, 99))
-random_ints_1to99.append(random.randint(1, 99))
-random_ints_1to99.append(random.randint(1, 99))
-random_ints_1to99.append(random.randint(1, 99))
-random_ints_1to99.append(random.randint(1, 99))
-random_ints_1to99.append(random.randint(1, 99))
-random_ints_1to99.append(random.randint(1, 99))
-random_ints_1to99.append(random.randint(1, 99))
+generate_numbers(1, 99, random_ints_1to99)
 for i in range(10):
     user_number = int(input("Enter an integer from 1 to 99: "))
     while random_ints_1to99[i] != user_number:
@@ -24,16 +22,7 @@ for i in range(10):
     print("you guessed it!")
 
 random_ints_1to49 = []
-random_ints_1to49.append(random.randint(1, 49))
-random_ints_1to49.append(random.randint(1, 49))
-random_ints_1to49.append(random.randint(1, 49))
-random_ints_1to49.append(random.randint(1, 49))
-random_ints_1to49.append(random.randint(1, 49))
-random_ints_1to49.append(random.randint(1, 49))
-random_ints_1to49.append(random.randint(1, 49))
-random_ints_1to49.append(random.randint(1, 49))
-random_ints_1to49.append(random.randint(1, 49))
-random_ints_1to49.append(random.randint(1, 49))
+generate_numbers(1, 49, random_ints_1to49)
 for i in range(10):
     user_number = int(input("Enter an integer from 1 to 49: "))
     while random_ints_1to49[i] != user_number:
