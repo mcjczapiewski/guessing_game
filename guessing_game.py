8162efd3f763d0b69a2a6747969d86b05d34457a
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-03-14 15:22:34 +0100

    Refactor for loops into function

--- guessing_game.py
+++ guessing_game.py
@@ -19,14 +19,18 @@ def check_user_input(user_number, list_of_numbers, i):
     print("you guessed it!")
 
 
+def guess_10_numbers(list_of_numbers, range_end):
+    for i in range(10):
+        user_number = int(
+            input("Enter an integer from 1 to " + str(range_end) + ": ")
+        )
+        check_user_input(user_number, list_of_numbers, i)
+
+
 random_ints_1to99 = []
 generate_numbers(1, 99, random_ints_1to99)
-for i in range(10):
-    user_number = int(input("Enter an integer from 1 to 99: "))
-    check_user_input(user_number, random_ints_1to99, i)
+guess_10_numbers(random_ints_1to99, 99)
 
 random_ints_1to49 = []
 generate_numbers(1, 49, random_ints_1to49)
-for i in range(10):
-    user_number = int(input("Enter an integer from 1 to 49: "))
-    check_user_input(user_number, random_ints_1to49, i)
+guess_10_numbers(random_ints_1to49, 49)
